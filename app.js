cfc97c4cb437819c7553330f967d8fb7982635ab
Naman <namansharma20ns02@gmail.com>
2024-07-10 12:13:51 +0530

    dashboard is added for the admin

--- app.js
+++ app.js
@@ -4,18 +4,37 @@ require("dotenv").config()//now we are able to use dotenv in our application
 
 const express = require("express")//THIS mean now e import th express server
 const expressLayout = require("express-ejs-layouts")
+const cookieParser = require("cookie-parser")//this for the cookie
+const MongoStore = require("connect-mongo")
 
+const session = require("express-session")
 const connectDB = require("./server/config/db")//now we are integrating the database here 
 const app = express()
 const PORT = 3000 || process.env.PORT;
 connectDB();//now using this our databse connects
 app.use(express.static("public"))//this allow us to not goback in the file just directly access all the file by simple put their parent file name by a single slash
 app.use(express.urlencoded({extended:true}))
-app.use(express.json())
-
+app.use(express.json())//a middle ware that wllow use to show the output in the json format 
+app.use(cookieParser("Keyboard cat"))//the thing inside the brackests would ebsame as that of the session thing as this is the new update
 //templating engine
 app.use(expressLayout)//we are using the library
 app.set("layout","./layouts/main")
+app.use(session({
+    secret:"Keyboard cat",//this is just a place holder
+    resave: false,
+    saveUninitialized:true,//this means that the session would create even though we don't add anything 
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_URL
+    }),
+}));
+// app.use(session({
+//     secret: "keyboard cat",
+//     resave: false,
+//     saveUninitialized:true,
+//     store: MongoStore.create({
+//         mongoUrl:process.env.MONGODB_URL
+//     })
+// }))
 app.set("view engine","ejs")
 
 
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,8 +3,45 @@ const express = require("express")
 const router = express.Router()
 const Post = require("../models/Post")//we are importing the post model
 const User = require("../models/User")//we are importing the post model
+const bcrypt = require("bcrypt")//encryption of the password 
+const jwt = require("jsonwebtoken")
+const adminLayout = "../views/layouts/admin";//import ing the admin layout 
+const jwtSecret = process.env.JWT_SECRET;
 
-const adminLayout = "../views/layouts/admin";
+
+
+/**check the login 
+ *  */ 
+
+
+const authMiddleware = (req,res,next) => {//next is used to pass the control to the next middleware 
+    const token = req.cookies.token;//extract the token from the cookies 
+
+    if(!token){
+        res.status(401).json({
+            mesaage:"Unauthorized"
+        })
+    }
+    try{
+        const decoded = jwt.verify(token,jwtSecret);//here we are verifying the token using the jwtsecret key 
+        req.userId = decoded.userId;//here userId is the data part that we store that means here we are using that userid that we store 
+        //in out jwt token and now we are checking that by decoding it 
+        next();
+    }
+    catch(error)
+    {
+        res.status(401).json({
+            mesaage:"Unauthorized"
+        })
+    }
+
+}
+
+
+
+
+
+
 
 
 /**GET
@@ -18,10 +55,11 @@ router.get('/admin', async(req,res) =>{
 
         const locals = {//this is a object 
             title: "Admin",
-            description : "simple blog created with nodejs , express and mongodb",
+            // description : "simple blog created with nodejs , express and mongodb",
         }
 
-        res.render("admin/index",{locals,layout: adminLayout})//fromhere we can render the local data 
+        res.render("admin/index",{locals,layout: adminLayout})//from here we can render the local data 
+        //rendering basically means that we are retriving the data and sending to the will the we put under the "" 
     }
     catch(error){
         console.log(error)
@@ -32,17 +70,146 @@ router.get('/admin', async(req,res) =>{
 /**Post admin chaeck again
  *  */ 
 
-
 router.post('/admin', async(req,res) =>{
     try{
-        const {username, password } = req.body;
-        console.log(req.body);
-        res.redirect("/admin")
+        const {username, password } = req.body;//we are requsting this and store it in the username and password 
+        const user = await User.findOne({username});
+        if(!user){
+            res.status(401).json({message:"invalid credentials"})
+        }
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+
+        if(!isPasswordValid){
+            res.status(401).json({mesaage:"invalid credential"})
+        }
+        const token = jwt.sign({userId:user._id},jwtSecret);//here i am creating the jwt token and the the data part would store the uerid here 
+        res.cookie("token",token,{httpOnly: true});//method to set the cookie in response 
+        //"token" this is the name of the cookie 
+        //httpsonly preven the client side to access the token
+        res.redirect("/dashboard")//in response the user get redirect to the dashboard 
         }
     catch(error){
         console.log(error)
     }
 });
+
+
+
+/**get the dashboard
+ *  */ 
+
+router.get('/dashboard',authMiddleware ,async(req,res) =>{
+    try{
+        const locals = {
+            title: "Dashboard",
+            description: "sample blog created by nodehs and mongodb"
+        }
+        const data = await Post.find();
+        res.render("admin/dashboard",{
+            locals,
+            data,
+            layout: adminLayout
+        })
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+
+
+/**GET admin create a new post or i would say add a new post 
+ *  */ 
+
+router.get('/add-post',authMiddleware ,async(req,res) =>{
+    try{
+        const locals = {
+            title: "Dashboard",
+            description: "sample blog created by nodehs and mongodb"
+        }
+        const data = await Post.find();
+        res.render("admin/add-post",{
+            locals,
+            layout: adminLayout
+        })
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+
+
+
+/**POST the things that we put in the data we have to put them in the database 
+ *  */ 
+
+router.post('/add-post',authMiddleware ,async(req,res) =>{
+    try{
+        try{
+            const newPost = new Post({
+                title: req.body.title,
+                body: req.body.body
+            })
+            await Post.create(newPost);
+            res.redirect("/dashboard")
+        }
+        catch(error){
+            console.log(error);
+        }
+    }
+    catch(error){
+        console.log(error);
+    }
+});
+
+
+
+
+
+// router.post('/admin', async(req,res) =>{
+//     try{
+//         const {username, password } = req.body;//we are requsting this and store it in the username and password 
+//         console.log(req.body);//we are printing it to confirm 
+//         res.redirect("/admin")//as a response we are coming back to teh same page 
+//         }
+//     catch(error){
+//         console.log(error)
+//     }
+// });
+
+/**register the admin 
+ *  */ 
+
+
+router.post('/register', async(req,res) =>{
+    try{
+        const {username, password } = req.body;//we are requsting this and store it in the username and password 
+        const hashedPassword = await bcrypt.hash(password,10);//this willl decrypt the passwrod 
+        //the parameters int he brypt the first one is the password that we want to decrypt the second one would be the number of rounds we want to run a cycle to convert the string into the hascode 
+//exmple naman-> jhdsbhdf -> sjbdcjhsdb ->sjdvcjhbdc so and so 10 times so it's so much hard to crack the pssword by brute force 
+        try{
+            const user = await User.create({username,password:hashedPassword});
+            res.status(201).json({message: "User Created",user});
+        }
+        catch(error){
+            if(error.code === 11000){//this represent the duplicacy that mean the user already exist
+                res.status(409).json({message:"user already in use"});
+            }
+            res.status(500).json({message:"internal server error"})
+        }
+    }
+    catch(error){
+        console.log(error)
+    }
+});
+
+
+
+
+
+
+
 
 
 
